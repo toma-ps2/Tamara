e2df808893553175a7025c69564e544b24b1c5ff
Manas <hayk_mkrtchyan_2016@mail.ru>
2023-03-09 21:09:48 +0300

    remove directory test

---
+++ db.php
@@ -0,0 +1,16 @@
+<?php
+try {
+
+	$option = [
+		// по умолчанию данные возвращать в виде ассоциативного массива
+		PDO::ATTR_DEFAULT_FETCH_MODE =>PDO::FETCH_ASSOC,
+		// кодировка для работы с БД
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+	];
+
+	$dbh = new PDO('mysql:dbname=nordic2023;host=localhost', 'root', 'root',$option);
+
+} catch (PDOException $e) {
+	die($e->getMessage());
+}
+
