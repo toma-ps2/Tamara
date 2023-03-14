b0dcc0a824a9dfff4fdde7cf46302d7bb06d665f
Admin <alexanderbalabas1993@gmail.com>
2023-03-14 21:02:17 +0300

    add fetch panel.js

--- js/panel.js
+++ js/panel.js
@@ -3,5 +3,28 @@ const addProduct = document.querySelector('input[type=submit]')
 
 addProduct.addEventListener('click',(Event)=>{
     Event.preventDefault()
-    
+    // product - id в теге form
+    let data = new FormData(product)
+
+    // посмотреть все поля обьекта data
+    // data.entries() - возвращает массив
+
+    // перебор обьектов 
+    //console.log(Object.values({name:'Alex',city:'Moscow'}))
+    //console.log(Object.keys({name:'Alex',city:'Moscow'}))
+    //console.log(Object.entries({name:'Alex',city:'Moscow'}))
+
+    // for of - перебираем массив
+    for (let [k,v] of data.entries())
+    {
+        //console.log(k,v)
+    }
+
+
+    fetch('handler.php',{
+        method:'POST',
+        body:data
+    })
+    .then(response=> response.text())
+   
 })
